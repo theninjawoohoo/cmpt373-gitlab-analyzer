c92eaafd3696eb6411827acc64c99620ce0e6fe7
jfitzmor <joshua_fitz-morris@sfu.ca>
2021-01-30 12:42:53 -0800

    center grid

--- packages/webapp/src/components/HomeLayout/index.tsx
+++ packages/webapp/src/components/HomeLayout/index.tsx
@@ -1,33 +1,31 @@
-import React from 'react';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import { useAuthContext } from '../../contexts/AuthContext';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
+import React from "react";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import { useAuthContext } from "../../contexts/AuthContext";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-
     title: {
-      fontSize: '2rem',
-      textAlign: 'center',
+      fontSize: "2rem",
+      textAlign: "center",
     },
 
     paperBlock: {
       padding: theme.spacing(2),
-      margin: 'auto',
+      margin: "auto",
       maxWidth: 300,
       maxHeight: 400,
     },
 
     center: {
-      textAlign: 'center',
-    }
-
-  }),
+      textAlign: "center",
+    },
+  })
 );
 
 const HomeLayout: React.FC = ({ children }) => {
@@ -35,13 +33,22 @@ const HomeLayout: React.FC = ({ children }) => {
   const { state: authState, dispatch } = useAuthContext();
   return (
     <>
-    {/* <Paper className={styles.paperBlock} elevation={3}> */}
-      <Grid container direction="column" justify="center" spacing={2}>
+      {/* <Paper className={styles.paperBlock} elevation={3}> */}
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        spacing={2}
+        style={{ minHeight: "100vh" }}
+      >
         <Grid item>
-          <Typography variant='h1' className={styles.title}>GitLab Analyzer</Typography>
+          <Typography variant="h1" className={styles.title}>
+            GitLab Analyzer
+          </Typography>
         </Grid>
         <Grid item>
-          <Link href='https://cas.sfu.ca?service=http://localhost:3000/login/sfu' >
+          <Link href="https://cas.sfu.ca?service=http://localhost:3000/login/sfu">
             <div className={styles.center}>
               <Button variant="contained" color="inherit">
                 Log in via SFU
@@ -50,7 +57,7 @@ const HomeLayout: React.FC = ({ children }) => {
           </Link>
         </Grid>
       </Grid>
-    {/* </Paper> */}
+      {/* </Paper> */}
     </>
   );
 };
