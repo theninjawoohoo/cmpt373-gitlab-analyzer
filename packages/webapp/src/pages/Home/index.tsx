80d3effb9112bd7dab3ea21d64dfd1a305265fc9
jfitzmor <joshua_fitz-morris@sfu.ca>
2021-02-01 20:22:46 -0800

    merge

--- packages/webapp/src/pages/Home/index.tsx
+++ packages/webapp/src/pages/Home/index.tsx
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import DefaultPageLayout from "../../components/HomePageLayout";
-
-const Home: React.FC = () => <DefaultPageLayout />;
-
-export default Home;
-=======
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import DefaultPageLayout from '../../components/DefaultPageLayout';
@@ -23,4 +15,3 @@ const Home: React.FC = ({ children }) => {
 
 export default Home;
 ;
->>>>>>> 3acd3cc4c1719d5034a389b2a363e43f4138a7d3
