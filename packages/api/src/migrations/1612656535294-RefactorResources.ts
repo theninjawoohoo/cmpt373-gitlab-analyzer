d58cfec76de3e221bd4ef11d2432e328be631fba
Jagrajan Bhullar <jsbhulla@sfu.ca>
2021-02-06 22:17:37 -0800

    fix bad migration

--- packages/api/src/migrations/1612656535294-RefactorResources.ts
+++ packages/api/src/migrations/1612656535294-RefactorResources.ts
@@ -4,6 +4,7 @@ export class RefactorResources1612656535294 implements MigrationInterface {
   name = 'RefactorResources1612656535294';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('repository', true);
     await queryRunner.query(
       `CREATE TABLE "repository" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "resource" jsonb NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "userId" uuid, CONSTRAINT "PK_b842c26651c6fc0b9ccd1c530e2" PRIMARY KEY ("id"))`,
     );
