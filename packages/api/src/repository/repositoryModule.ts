88f1caa7b6998c7846ae11e580e07f5029936ab9
Ha Thu Nguyen <hathun@sfu.ca>
2021-02-07 10:53:35 -0800

    Delete repository controller module in src folder.
    Remove repository controller module in the src folder added before and use the repository controller module in src/gitlab/repository instead.

--- packages/api/src/repository/repositoryModule.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RepositoryController } from './repository.controller';
-
-@Module({
-  controllers: [RepositoryController],
-})
-export class RepositoryModule {}
