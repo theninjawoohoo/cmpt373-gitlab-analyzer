bd2a60c8a884a4bed42b20e591159ae59d211f26
jfitzmor <joshua_fitz-morris@sfu.ca>
2021-02-14 12:21:01 -0800

    settings page has form added and will verify token

--- packages/webapp/src/components/SettingsPageLayout/index.tsx
+++ packages/webapp/src/components/SettingsPageLayout/index.tsx
@@ -1,61 +1,49 @@
-import Box from '@material-ui/core/Box';
-import { useState } from 'react';
-import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import Toolbar from '@material-ui/core/Toolbar';
+import { useVerifyToken } from '../../api/token';
 import { useAuthContext } from '../../contexts/AuthContext';
-import UndecoratedLink from '../UndecoratedLink';
-import Form from '../Common/Form';
+import NavBar from '../NavBar';
+import Form from './Form';
 const useStyles = makeStyles(() => ({
   title: {
     flexGrow: 1,
     fontSize: '2rem',
   },
+  gridDimensions: {
+    minHeight: '100vh',
+    display: 'grid',
+    gridTemplateColumns: 'auto 1fr',
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
 }));
 
-const SettingsPageLayout: React.FC = ({ children }) => {
+const SettingsPageLayout: React.FC = () => {
   const styles = useStyles();
-  const { state: authState, dispatch } = useAuthContext();
-  const [apiKey, setApiKey] = useState('');
+  const { user } = useAuthContext();
+  const hasValidApi = useVerifyToken();
+  const message = hasValidApi
+    ? 'You have a valid API Key. To update your API Key, enter a new one below:'
+    : 'You have an invalid API Key. Please enter a valid API Key below:';
   return (
     <>
-      <AppBar>
-        <Container>
-          <Toolbar>
-            <Typography variant='h2' className={styles.title}>
-              GitLab Analyzer
-            </Typography>
-            <UndecoratedLink to='/'>
-              <Button color='inherit'>Home</Button>
-            </UndecoratedLink>
-            {authState?.user?.sfuId ? (
-              <>
-                <UndecoratedLink to='/profile'>
-                  <Button color='inherit'>{authState.user.sfuId}</Button>
-                </UndecoratedLink>
-                <Button
-                  color='inherit'
-                  onClick={() => dispatch({ type: 'LOGOUT' })}
-                >
-                  Logout
-                </Button>
-              </>
-            ) : (
-              <UndecoratedLink to='/login'>
-                <Button color='inherit'>Login</Button>
-              </UndecoratedLink>
-            )}
-          </Toolbar>
-        </Container>
-      </AppBar>
-      <Box height='4rem' />
-      {children}
-      <h1> Settings Page</h1>
-      <Form apiKey={apiKey} setApiKey={setApiKey} />
+      <div className={styles.gridDimensions}>
+        {user?.sfuId ? (
+          <>
+            <NavBar username={user.sfuId} />
+            <div className={styles.form}>
+              <h2>{message}</h2>
+              <Form />
+            </div>
+          </>
+        ) : (
+          <NavBar username={'Guest'} />
+        )}
+      </div>
     </>
   );
 };
