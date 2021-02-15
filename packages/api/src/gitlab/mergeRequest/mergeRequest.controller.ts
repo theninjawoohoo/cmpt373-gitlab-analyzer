ced89cb63abbfff70993b553e5b0f7bfc778bd34
Maggie <zhiqi_qiao@sfu.ca>
2021-02-14 22:42:42 -0800

    Use IdPram as param for checking valid id
    Signed-off-by: Maggie <zhiqi_qiao@sfu.ca>

--- packages/api/src/gitlab/mergeRequest/mergeRequest.controller.ts
+++ packages/api/src/gitlab/mergeRequest/mergeRequest.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
+import { Controller, Get, HttpCode, NotFoundException ,Param, Post } from '@nestjs/common';
 import { MergeRequestService } from './mergeRequest.service';
 import { GitlabToken } from '../../auth/decorators/gitlab-token.decorator';
 import { RepositoryService } from '../repository/repository.service';
+import { IdParam } from 'src/common/id-param';
 
 @Controller('mergerequest')
 export class MergeRequestController {
@@ -10,26 +11,38 @@ export class MergeRequestController {
     private readonly repositoryService: RepositoryService,
   ) {}
 
-  @Get('/repository/:repository_id')
+  @Get('/repository/:id')
   async findAllForRepository(
-    @Param('repository_id') repoId: string
+    @Param() { id }: IdParam
   ){
-    const repository = await this.repositoryService.findOne(repoId);
-    return this.mergeRequestService.findAllForRepository(repository);
+    const repository = await this.repositoryService.findOne(id);
+    if (repository) {
+      return this.mergeRequestService.findAllForRepository(repository);
+    }
+    throw new NotFoundException(`Could not find merge request for repository with id: ${id}`);
   }
 
   @Get(':id')
-  findOne(@Param('id') mergeRequestId: string) {
-    return this.mergeRequestService.findOne(mergeRequestId);
+  async findOne(@Param() { id }: IdParam) {
+    const mergeRequest = await this.mergeRequestService.findOne(id);
+    if (mergeRequest) {
+      return mergeRequest;
+    }
+    throw new NotFoundException(`Could not find a merge request with id: ${id}`);
   }
 
-  @Post('/repository/:repository_id')
+  @Post('/repository/:id')
   @HttpCode(204)
   async fetchMergeRequests(
-    @Param('repository_id') repoId: string,
+    @Param() { id }: IdParam,
     @GitlabToken() token: string,
   ) {
-    const repository = await this.repositoryService.findOne(repoId);
-    await this.mergeRequestService.fetchForRepository(repository, token);
+    const repository = await this.repositoryService.findOne(id);
+    if (repository) {
+      await this.mergeRequestService.fetchForRepository(repository, token);
+    }
+    else{
+      throw new NotFoundException(`Could not fetch merge request for repository with id: ${id}`);
+    }
   }
 }
