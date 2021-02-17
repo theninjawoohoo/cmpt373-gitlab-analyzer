00c9f2552efbb7eab1413176f88d32a7a6f87b0c
Ha Thu Nguyen <hathun@sfu.ca>
2021-02-16 17:08:28 -0800

    Improve students' dropdown menu design using Material UI.

--- packages/webapp/src/components/DropdownMenu/index.tsx
+++ packages/webapp/src/components/DropdownMenu/index.tsx
@@ -1,41 +1,54 @@
 import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
-interface DropdownMenuProps {
-  selectedItem?: string;
-}
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    menu: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectLabel: {
+      marginTop: theme.spacing(1),
+    },
+  }),
+);
 
-const DropdownMenu: React.FC<DropdownMenuProps> = (DropdownMenuProps) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [selected, setSelected] = React.useState('');
+const StudentDropdownMenu = () => {
+  const classes = useStyles();
+  const [studentName, setStudentName] = React.useState('');
 
-  const handleChange = (event) => {
-    setSelected(event.target.value);
-  };
-
-  const handleSubmit = () => {
-    setSelected('submit');
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setStudentName(event.target.value as string);
   };
 
   return (
     <div>
-      <h1>Hello!</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Pick a student:
-          <select
-            value={DropdownMenuProps.selectedItem}
-            onChange={handleChange}
-          >
-            <option value='firstStudent'>First Student</option>
-            <option value='secondStudent'>Second Student</option>
-            <option value='thirdStudent'>Third Student</option>
-            <option value='fourthStudent'>Fourth Student</option>
-          </select>
-        </label>
-        <input type='submit' value='Submit' />
-      </form>
+      <FormControl variant='outlined' className={classes.menu}>
+        <InputLabel shrink id='student-dropdown-menu-input-label'>
+          Show results for:
+        </InputLabel>
+        <Select
+          labelId='student-dropdown-menu-input-label'
+          id='student-dropdown-menu-options'
+          value={studentName}
+          onChange={handleChange}
+          displayEmpty
+          className={classes.selectLabel}
+          autoWidth
+        >
+          <MenuItem value=''>All students</MenuItem>
+          <MenuItem value={40}>First Student</MenuItem>
+          <MenuItem value={10}>Second Student</MenuItem>
+          <MenuItem value={20}>Third Student</MenuItem>
+          <MenuItem value={30}>Fourth Student</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
 };
 
-export default DropdownMenu;
+export default StudentDropdownMenu;
