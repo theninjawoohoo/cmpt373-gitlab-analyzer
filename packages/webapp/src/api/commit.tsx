7dfd7aab970373ddd580a4fa89c887d0e0a5f363
Jagrajan Bhullar <jsbhulla@sfu.ca>
2021-02-20 18:48:39 -0800

    add front end api hook for getting daily counts

--- packages/webapp/src/api/commit.tsx
+++ packages/webapp/src/api/commit.tsx
@@ -1,5 +1,25 @@
-import { useApiQuery, SearchResults } from './base';
+import { useApiQuery, SearchResults, usePaginatedQuery } from './base';
 import { Commit } from '@ceres/types';
+
+interface DailyCommitSearchParams {
+  repository: string;
+  author_email?: string;
+  start_date?: string;
+  end_date?: string;
+}
+
+export function useCommitDailyCounts(
+  params: DailyCommitSearchParams,
+  page?: number,
+  pageSize?: number,
+) {
+  return usePaginatedQuery<Commit.DailyCount>(
+    '/commit/daily_count',
+    params,
+    page,
+    pageSize,
+  );
+}
 
 export function useCommitsForMergeRequest(mergeRequestId: string) {
   return useApiQuery<SearchResults<Commit>>(
