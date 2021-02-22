18415236aa908f774296698a665b08a66b1642e6
Jagrajan Bhullar <jsbhulla@sfu.ca>
2021-02-21 20:44:47 -0800

    add details to operation progress body

--- packages/webapp/src/pages/Operations/components/OperationProgress.tsx
+++ packages/webapp/src/pages/Operations/components/OperationProgress.tsx
@@ -3,13 +3,15 @@ import { AccordionDetails } from '@material-ui/core';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import React from 'react';
+import { ApiResource } from '../../../api/base';
 import StageProgress from './StageProgress';
 
 interface OperationProgressProps {
-  operation: Operation;
+  operation: ApiResource<Operation>;
 }
 
 const OperationProgress: React.FC<OperationProgressProps> = ({ operation }) => {
@@ -22,7 +24,14 @@ const OperationProgress: React.FC<OperationProgressProps> = ({ operation }) => {
           justifyContent='space-between'
           alignItems='center'
         >
-          <Typography>{operation.type}</Typography>
+          <div>
+            <Typography>{operation.type}</Typography>
+            <Typography variant='body2'>
+              {new Date(operation.meta.createdAt).toDateString()}
+              {' - '}
+              {new Date(operation.start_time).toLocaleTimeString('en-US')}
+            </Typography>
+          </div>
           <Box>
             {operation.stages.map((stage) => (
               <StageProgress key={stage.name} stage={stage} />
@@ -30,7 +39,20 @@ const OperationProgress: React.FC<OperationProgressProps> = ({ operation }) => {
           </Box>
         </Box>
       </AccordionSummary>
-      <AccordionDetails>Something</AccordionDetails>
+      <AccordionDetails>
+        <Grid container spacing={2}>
+          {operation?.stages.map((stage) => (
+            <>
+              <Grid item xs={6}>
+                <Typography>{stage.name}</Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography>{stage.status}</Typography>
+              </Grid>
+            </>
+          ))}
+        </Grid>
+      </AccordionDetails>
     </Accordion>
   );
 };
