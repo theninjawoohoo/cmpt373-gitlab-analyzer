fe3bda6154a2ad7c163c73a6555bda51e4b4903a
Ha Thu Nguyen <hathun@sfu.ca>
2021-03-04 02:17:41 -0800

    Add controller for Notes.

--- packages/api/src/gitlab/repository/notes/notes.controller.ts
+++ packages/api/src/gitlab/repository/notes/notes.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { NoteService } from './notes.service';
+import { IdParam } from '../../../common/id-param';
+import { RepositoryService } from '../repository.service';
+import { GitlabToken } from '../../../auth/decorators/gitlab-token.decorator';
 
 @Controller('notes')
-export class NotesController {}
+export class NotesController {
+  constructor(
+    private readonly noteService: NoteService,
+    private readonly repositoryService: RepositoryService,
+  ) {}
+
+  @Get('/repository/:id')
+  async findAllForRepository(@Param() { id }: IdParam) {
+    const repository = await this.repositoryService.findOne(id);
+    if (repository) {
+      return this.noteService.findAllForRepository(repository);
+    }
+    throw new NotFoundException(
+      `Could not find any notes for repository with id: ${id}`,
+    );
+  }
+
+  @Get(':id')
+  async findOne(@Param() { id }: IdParam) {
+    const note = await this.noteService.findOne(id);
+    if (note) {
+      return note;
+    }
+    throw new NotFoundException(`Could not find a note with id: ${id}`);
+  }
+
+  @Post('/repository/:id')
+  @HttpCode(204)
+  async fetchNotes(@Param() { id }: IdParam, @GitlabToken() token: string) {
+    const repository = await this.repositoryService.findOne(id);
+    if (repository) {
+      await this.noteService.fetchForRepository(repository, token);
+    } else {
+      throw new NotFoundException(
+        `Could not fetch notes for repository with id: ${id}`,
+      );
+    }
+  }
+}
