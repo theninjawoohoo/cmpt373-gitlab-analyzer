ec33ad71b9ca61e2a7fe167407767223fb40dd1b
Ha Thu Nguyen <hathun@sfu.ca>
2021-03-06 02:29:15 -0800

    Rename notes.entity to note.entity
    Notes is renamed to Note since singular form should be used for a database entity.

--- packages/api/src/gitlab/repository/note/note.entity.ts
+++ packages/api/src/gitlab/repository/note/note.entity.ts
@@ -2,7 +2,6 @@ import { Note as NotesResource } from '@ceres/types';
 import { Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { BaseEntity } from '../../../common/base-entity';
 import { MergeRequest } from '../../merge-request/merge-request.entity';
-import { Repository } from '../repository.entity';
 // import { Issue } from '../issues/issues.entity';
 
 @Entity('merge_request_note')
