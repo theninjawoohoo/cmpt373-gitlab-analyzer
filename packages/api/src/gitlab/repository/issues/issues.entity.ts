4cafc2fee5da22e86e9cb4ce5110d1f6e448e604
Ha Thu Nguyen <hathun@sfu.ca>
2021-03-07 23:09:50 -0800

    Uncomment out code in Issue.
    Uncomment out the code in issues.entity because it will be needed in the future, and because no commented out code should be merged into master.

--- packages/api/src/gitlab/repository/issues/issues.entity.ts
+++ packages/api/src/gitlab/repository/issues/issues.entity.ts
@@ -1,12 +1,6 @@
-// import { Issue as IssuesResource } from '@ceres/types';
-// import { Entity, ManyToMany, OneToMany } from 'typeorm';
-// import { BaseEntity } from '../../../common/base-entity';
-// import { Repository } from '../repository.entity';
-// import { RepositoryMember } from '../repository-member/repository-member.entity';
-// import { Note } from '../note/note.entity';
-//
-// @Entity('issue')
-// export class Issue extends BaseEntity<IssuesResource> {
-//   // @OneToMany(() => Note, (note) => note.issue)
-//   // note: Note[];
-// }
+import { Issue as IssuesResource } from '@ceres/types';
+import { Entity } from 'typeorm';
+import { BaseEntity } from '../../../common/base-entity';
+
+@Entity('issue')
+export class Issue extends BaseEntity<IssuesResource> {}
