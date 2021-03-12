621034a57bf39d66bc1375513af03a9c0b55ef8d
Jagrajan Bhullar <jsbhulla@sfu.ca>
2021-03-12 14:45:09 -0800

    [api] create scoring config module

---
+++ packages/api/src/scoring-config/scoring-config.controller.ts
@@ -0,0 +1,29 @@
+import { ScoringConfig } from '@ceres/types';
+import { Body } from '@nestjs/common';
+import { Put } from '@nestjs/common';
+import { Param } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { IdParam } from '../common/id-param';
+import { Auth } from '../auth/decorators/auth.decorator';
+import { VerifiedUser } from '../auth/types/VerifiedUser';
+import { ScoringConfigService } from './scoring-config.service';
+
+@Controller('scoring_config')
+export class ScoringConfigController {
+  constructor(private readonly scoringConfigService: ScoringConfigService) {}
+
+  @Get()
+  search(@Auth() { user }: VerifiedUser) {
+    return this.scoringConfigService.search({ user });
+  }
+
+  @Post()
+  create(@Auth() { user }: VerifiedUser, @Body() resource: ScoringConfig) {
+    return this.scoringConfigService.create({ resource, user });
+  }
+
+  @Put(':id')
+  update(@Param() { id }: IdParam, @Body() resource: ScoringConfig) {
+    return this.scoringConfigService.update({ id, resource });
+  }
+}
---
+++ packages/api/src/scoring-config/scoring-config.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ScoringConfigService } from './scoring-config.service';
+import { ScoringConfigController } from './scoring-config.controller';
+import { ScoringConfig } from './scoring-config.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ScoringConfig])],
+  providers: [ScoringConfigService],
+  controllers: [ScoringConfigController],
+})
+export class ScoringConfigModule {}
