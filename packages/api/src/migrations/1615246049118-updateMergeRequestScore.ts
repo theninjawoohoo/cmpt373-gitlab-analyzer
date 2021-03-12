c14552ab0c3c3e1c25bfe636292275cf5ec3cc87
Jagrajan Bhullar <jsbhulla@sfu.ca>
2021-03-12 14:44:11 -0800

    [api] fix scoring column migrations

--- packages/api/src/migrations/1615246049118-updateMergeRequestScore.ts
+++ packages/api/src/migrations/1615246049118-updateMergeRequestScore.ts
@@ -6,12 +6,6 @@ export class updateMergeRequestScore1615246049118
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "merge_request" DROP COLUMN "diffScore"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "merge_request" DROP COLUMN "commitScore"`,
-    );
-    await queryRunner.query(
       `ALTER TABLE "merge_request" ADD "diff_score" double precision`,
     );
     await queryRunner.query(
@@ -25,12 +19,6 @@ export class updateMergeRequestScore1615246049118
     );
     await queryRunner.query(
       `ALTER TABLE "merge_request" DROP COLUMN "diff_score"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "merge_request" ADD "commitScore" double precision`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "merge_request" ADD "diffScore" double precision`,
     );
   }
 }
