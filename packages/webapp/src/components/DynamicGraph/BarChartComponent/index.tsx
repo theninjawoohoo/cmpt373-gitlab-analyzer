1e3332899392b0ad043a25712a402068c4bee50e
xhzhong <xuehanzhong@gmail.com>
2021-03-13 16:55:30 -0800

    link tab to change graph according to type

---
+++ packages/webapp/src/components/DynamicGraph/BarChartComponent/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+
+interface BarChartProps {
+  graphData: any[];
+  graphType: number;
+}
+
+const DynamicBarChart: React.FC<BarChartProps> = (BarChartProps) => {
+  if (BarChartProps.graphType == 0) {
+    return (
+      <BarChart width={1000} height={500} data={BarChartProps.graphData}>
+        <XAxis dataKey='date' />
+        <YAxis />
+        <Tooltip />
+        <Legend layout='vertical' align='right' verticalAlign='top' />
+        <Bar dataKey='commitCount' name='Commits' stackId='a' fill='#0A4D63' />
+        <Bar
+          dataKey='mergeCount'
+          name='Merge Requests'
+          stackId='a'
+          fill='#e37500'
+        />
+      </BarChart>
+    );
+  } else if (BarChartProps.graphType == 1) {
+    return (
+      <BarChart width={1000} height={500} data={BarChartProps.graphData}>
+        <XAxis dataKey='date' />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey='score' name='Score' fill='#0A4D63' />
+      </BarChart>
+    );
+  } else {
+    return (
+      <BarChart width={1000} height={500} data={BarChartProps.graphData}>
+        <XAxis dataKey='date' />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey='wordCount' name='Word Counts' fill='#0A4D63' />
+      </BarChart>
+    );
+  }
+};
+
+export default DynamicBarChart;
