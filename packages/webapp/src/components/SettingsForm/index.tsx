2acc094b7ccb0b5b367b24f1330f544e83d1fc7f
xhzhong <xuehanzhong@gmail.com>
2021-03-22 18:30:58 -0700

    add back button on scoring and api pages

--- packages/webapp/src/components/SettingsForm/index.tsx
+++ packages/webapp/src/components/SettingsForm/index.tsx
@@ -6,17 +6,12 @@ import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import DefaultPageTitleFormat from '../DefaultPageTitleFormat';
 import { Typography } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import CancelIcon from '@material-ui/icons/Cancel';
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles(() => ({
-  // title: {
-  //   flexGrow: 1,
-  //   fontSize: '2rem',
-  // },
-  // gridDimensions: {
-  //   minHeight: '100vh',
-  //   display: 'grid',
-  //   gridTemplateColumns: 'auto 1fr',
-  // },
   form: {
     display: 'flex',
     flexDirection: 'column',
@@ -28,12 +23,22 @@ const useStyles = makeStyles(() => ({
 const SettingsForm: React.FC = () => {
   const styles = useStyles();
   const hasValidApi = useVerifyToken();
+  const { push } = useHistory();
   const message = hasValidApi
     ? 'You have a valid API Key. To update your API Key, enter a new one below:'
     : 'You have an invalid API Key. Please enter a valid API Key below:';
   return (
     <Container>
-      <DefaultPageTitleFormat>API Key</DefaultPageTitleFormat>
+      <Grid container alignItems='center' justify='space-between'>
+        <Grid item>
+          <DefaultPageTitleFormat>API Key</DefaultPageTitleFormat>
+        </Grid>
+        <Grid item>
+          <IconButton onClick={() => push('/settings')}>
+            <CancelIcon fontSize='large' />
+          </IconButton>
+        </Grid>
+      </Grid>
       <Box my={15}>
         <div className={styles.form}>
           <Typography variant='h5'>{message}</Typography>
--- packages/webapp/src/pages/Settings/index.tsx
+++ packages/webapp/src/pages/Settings/index.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
 import DefaultPageLayout from '../../components/DefaultPageLayout';
 import DefaultPageTitleFormat from '../../components/DefaultPageTitleFormat';
-// import { Link } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-// import Paper from '@material-ui/core/Paper';
-// import Typography from '@material-ui/core/Typography';
-// import { Button } from '@material-ui/core';
 import LinkCard from '../RepositoryHomePage/components/LinkCard';
 
-// const useStyles = makeStyles(() => ({
-//   paper: {
-//     padding: '15px',
-//     textAlign: 'left',
-//   },
-// }));
-
 const SettingsPage: React.FC = () => {
-  // const styles = useStyles();
   return (
     <div>
       <DefaultPageLayout>
@@ -28,12 +15,12 @@ const SettingsPage: React.FC = () => {
               <DefaultPageTitleFormat>Settings</DefaultPageTitleFormat>
             </Grid>
             <Grid item xs={12}>
-              <LinkCard color='#EFFFC0' icon='api' to={`/api`}>
+              <LinkCard color='#edd4ff' icon='api' to={`/api`}>
                 Change API Key
               </LinkCard>
             </Grid>
             <Grid item xs={12}>
-              <LinkCard color='#FFECFB' icon='scoring' to={`/scoring`}>
+              <LinkCard color='#ccdfff' icon='scoring' to={`/scoring`}>
                 Score Config
               </LinkCard>
             </Grid>
