813a76ea5b3d0b9d3cfffd122204a38d2ccedc4e
Ha Thu Nguyen <hathun@sfu.ca>
2021-04-05 01:46:42 -0700

    Attempt to populate note data by using api calls.

--- packages/webapp/src/pages/Test/index.tsx
+++ packages/webapp/src/pages/Test/index.tsx
@@ -5,9 +5,11 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import { Paper } from '@material-ui/core';
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+// import { DateTime } from 'luxon';
+// import { useFilterContext } from '../../contexts/FilterContext';
+// import { useGetMergeRequestNotes, useGetIssueNotes } from '../../api/note';
+import NotePaper from './NotePaper';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -27,13 +29,91 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-const Comment = () => {
+// const getMergeRequestNoteData = (date: DateTime, mergeRequestNotes: any[]) => {
+//   let filteredMergeRequestNotes: any[];
+//
+//   for (const result of mergeRequestNotes) {
+//     if (DateTime.fromISO(result.created_at).hasSame(date, 'day')) {
+//       filteredMergeRequestNotes.push(result);
+//     }
+//   }
+//
+//   return {
+//     date: date.toLocaleString(DateTime.DATE_SHORT),
+//     filteredMergeRequestNotes,
+//   };
+// };
+//
+// const getIssueNoteData = (date: DateTime, issueNotes: any[]) => {
+//   let filteredIssueNotes: any[];
+//
+//   for (const result of issueNotes) {
+//     if (DateTime.fromISO(result.date).hasSame(date, 'day')) {
+//       filteredIssueNotes.push(result);
+//     }
+//   }
+//   return {
+//     date: date.toLocaleString(DateTime.DATE_SHORT),
+//     filteredIssueNotes,
+//   };
+// };
+
+const Comment: React.FC = () => {
   const classes = useStyles();
 
-  const [commentType, setCommentType] = useState(0);
+  // const { startDate, endDate, emails } = useFilterContext();
+  // const { data: mergeRequestNotes } = useGetMergeRequestNotes(
+  //   {
+  //     author_email: emails,
+  //     start_date: startDate,
+  //     end_date: endDate,
+  //   },
+  //   0,
+  //   9000,
+  // );
+  //
+  // const { data: issueNotes } = useGetIssueNotes(
+  //   {
+  //     author_email: emails,
+  //     start_date: startDate,
+  //     end_date: endDate,
+  //   },
+  //   0,
+  //   9000,
+  // );
+
+  const [noteType, setNoteType] = useState(0);
+  // const [noteData, setNoteData] = useState([]);
+
+  // useEffect(() => {
+  //   if (startDate && endDate) {
+  //     let date = DateTime.fromISO(startDate);
+  //     const countsByDay = [];
+  //     if (noteType == 0) {
+  //       do {
+  //         countsByDay.push(
+  //           getMergeRequestNoteData(date, mergeRequestNotes?.results || []),
+  //         );
+  //         date = date.plus({ days: 1 });
+  //       } while (date <= DateTime.fromISO(endDate));
+  //     } else {
+  //       do {
+  //         countsByDay.push(getIssueNoteData(date, issueNotes?.results || []));
+  //         date = date.plus({ days: 1 });
+  //       } while (date <= DateTime.fromISO(endDate));
+  //     }
+  //     setNoteData(countsByDay);
+  //   }
+  // }, [
+  //   noteType,
+  //   mergeRequestNotes?.results,
+  //   issueNotes?.results,
+  //   startDate,
+  //   endDate,
+  // ]);
 
   const handleTabs = (event: React.ChangeEvent<unknown>, newType: number) => {
-    setCommentType(newType);
+    setNoteType(newType);
   };
 
   return (
@@ -42,7 +122,7 @@ const Comment = () => {
         <DefaultPageTitleFormat>Comments</DefaultPageTitleFormat>
         <Box my={2} className={classes.root}>
           <Tabs
-            value={commentType}
+            value={noteType}
             onChange={handleTabs}
             indicatorColor='primary'
             textColor='primary'
@@ -59,121 +139,7 @@ const Comment = () => {
           alignItems={'stretch'}
           spacing={1}
         >
-          <Paper elevation={3} className={classes.paper}>
-            <Grid justify={'space-between'} alignItems={'center'}>
-              <Typography>
-                <Box fontSize={18} paddingBottom={1.5}>
-                  On merge request{' '}
-                  <Box fontWeight='fontWeightBold' display='inline'>
-                    Link-to-merge-request
-                  </Box>
-                </Box>
-              </Typography>
-            </Grid>
-            <Grid
-              alignItems={'flex-start'}
-              container
-              direction={'row'}
-              spacing={2}
-            >
-              <Grid
-                item
-                xs={2}
-                style={{
-                  display: 'flex',
-                  justifyContent: 'flex-end',
-                }}
-              >
-                <Typography>
-                  <Box fontWeight='fontWeightBold' display='inline'>
-                    Author X
-                  </Box>{' '}
-                  wrote:
-                </Typography>
-              </Grid>
-              <Grid item xs={8}>
-                <Typography>
-                  This should display the content body of the code review from a
-                  merge request or commit code diff, or the note issue on an
-                  issue.
-                </Typography>
-                <Typography variant={'body2'} align={'right'}>
-                  <Box fontSize={16} fontStyle={'italic'} marginTop={2}>
-                    Date and time of the comment
-                  </Box>
-                </Typography>
-              </Grid>
-              <Grid item xs={2}>
-                <Typography align={'right'}>
-                  <Box
-                    fontWeight={'fontWeightBold'}
-                    fontSize={20}
-                    marginRight={2}
-                  >
-                    Word Count
-                  </Box>
-                </Typography>
-              </Grid>
-            </Grid>
-          </Paper>
-
-          <Paper elevation={3} className={classes.paper}>
-            <Grid justify={'space-between'} alignItems={'center'}>
-              <Typography>
-                <Box fontSize={18} paddingBottom={1.5}>
-                  On merge request{' '}
-                  <Box fontWeight='fontWeightBold' display='inline'>
-                    Link-to-merge-request
-                  </Box>
-                </Box>
-              </Typography>
-            </Grid>
-            <Grid
-              alignItems={'flex-start'}
-              container
-              direction={'row'}
-              spacing={2}
-            >
-              <Grid
-                item
-                xs={2}
-                style={{
-                  display: 'flex',
-                  justifyContent: 'flex-end',
-                }}
-              >
-                <Typography>
-                  <Box fontWeight='fontWeightBold' display='inline'>
-                    Author X
-                  </Box>{' '}
-                  wrote:
-                </Typography>
-              </Grid>
-              <Grid item xs={8}>
-                <Typography>
-                  This should display the content body of the code review from a
-                  merge request or commit code diff, or the note issue on an
-                  issue.
-                </Typography>
-                <Typography variant={'body2'} align={'right'}>
-                  <Box fontSize={16} fontStyle={'italic'} marginTop={2}>
-                    Date and time of the comment
-                  </Box>
-                </Typography>
-              </Grid>
-              <Grid item xs={2}>
-                <Typography align={'right'}>
-                  <Box
-                    fontWeight={'fontWeightBold'}
-                    fontSize={20}
-                    marginRight={2}
-                  >
-                    Word Count
-                  </Box>
-                </Typography>
-              </Grid>
-            </Grid>
-          </Paper>
+          <NotePaper noteType={noteType} />
         </Grid>
       </Container>
     </>
