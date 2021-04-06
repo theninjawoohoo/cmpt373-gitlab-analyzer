c0a166076d92ecb24fab6c0789cde662eb8b7ed3
Andy Wu <wuandyw@sfu.ca>
2021-04-05 21:00:05 -0700

    Fixed Group backend

--- packages/api/src/group/group.controller.ts
+++ packages/api/src/group/group.controller.ts
@@ -1,14 +1,33 @@
 import { GroupConfig } from '@ceres/types';
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Put } from '@nestjs/common';
+import { Param } from '@nestjs/common';
+import { IdParam } from '../common/id-param';
 import { Body } from '@nestjs/common';
 import { VerifiedUser } from '../auth/types/VerifiedUser';
 import { Auth } from '../auth/decorators/auth.decorator';
 import { GroupService } from './group.service';
+import { paginatedToResponse } from '../common/pagination';
 
 @Controller('group')
 export class GroupController {
-  //   @Post()
-  //   create(@Auth() { user }: VerifiedUser, @Body() resource: GroupConfig) {
-  //     return this.groupService.create({ resource, user });
-  //   }
+  constructor(private readonly groupService: GroupService) {}
+  @Get()
+  search(@Auth() { user }: VerifiedUser) {
+    return paginatedToResponse(this.groupService.search({ user }));
+  }
+
+  @Get(':id')
+  findOne(@Param() { id }: IdParam) {
+    return this.groupService.findOne(id);
+  }
+
+  @Post()
+  create(@Auth() { user }: VerifiedUser, @Body() resource: GroupConfig) {
+    return this.groupService.create({ resource, user });
+  }
+
+  @Put(':id')
+  update(@Param() { id }: IdParam, @Body() resource: GroupConfig) {
+    return this.groupService.update({ id, resource });
+  }
 }
--- packages/api/src/group/group.module.ts
+++ packages/api/src/group/group.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GroupController } from './group.controller';
 import { GroupService } from './group.service';
+import { GroupConfig } from './group.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([GroupConfig])],
   controllers: [GroupController],
-  providers: [GroupService]
+  providers: [GroupService],
+  exports: [GroupService],
 })
 export class GroupModule {}
