df5c0f3a9ba374216689d1e444ca4b352665db64
jfitzmor <joshua_fitz-morris@sfu.ca>
2021-04-10 14:25:33 -0700

    simplify sql query

--- packages/api/src/gitlab/repository/commit/commit.service.ts
+++ packages/api/src/gitlab/repository/commit/commit.service.ts
@@ -69,12 +69,9 @@ export class CommitService extends BaseService<
     }
 
     if (filters.not_associated_with_any_mr && filters.repository) {
-      query.andWhere(
-        `commit.id IN (SELECT commit.id FROM commit WHERE repository_id = :repository_id)`,
-        {
-          repository_id: filters.repository,
-        },
-      );
+      query.andWhere(`commit.repository_id = :repository_id`, {
+        repository_id: filters.repository,
+      });
       query.andWhere(
         `commit.id NOT IN (SELECT "commitId" FROM merge_request_commits_commit)`,
       );
