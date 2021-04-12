6004e25221c82c7132f61f0051d3e5e51cbe1ec7
xhzhong <xuehanzhong@gmail.com>
2021-04-11 17:30:37 -0700

    add to top button to setup and comment page

--- packages/webapp/src/pages/Comment/index.tsx
+++ packages/webapp/src/pages/Comment/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import CommentList from '../../components/CommentList';
+import ScrollToTop from '../../components/Common/ScrollToTop';
 import DefaultPageLayout from '../../components/DefaultPageLayout';
 
 const CommentPage: React.FC = () => {
   return (
     <DefaultPageLayout>
       <div>
+        <ScrollToTop />
         <CommentList />
       </div>
     </DefaultPageLayout>
